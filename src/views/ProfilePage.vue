422e862889f962624b2eee5866c36015cfdce975
rifqi <rifqidias6@gmail.com>
2022-09-09 11:35:17 +0700

    Initial commit

---
+++ src/views/ProfilePage.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+
+    <b-row>
+<div class="col col-12 card">
+  <div class="media">
+  <img src="../assets/rifqi.jpeg" class="mr-3" alt="...">
+  <div class="media-body">
+    <h5 class="mt-0">Media heading</h5>
+    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+
+    <div class="media mt-3">
+      <a class="mr-3" href="#">
+        <img src="../assets/rifqi.jpeg" class="mr-3" alt="...">
+      </a>
+      <div class="media-body">
+        <h5 class="mt-0">Media heading</h5>
+        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+    </b-row>
+  </div>
+</template>
+  
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+
+export default {
+  name: 'profilePage',
+  components: {}
+}
+</script>
+  
+<style scoped>
+    .user-pict{
+      border-radius: 200%;
+      height: 80px;
+      width: 80px;
+    }
+
+</style>
